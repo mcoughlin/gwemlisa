4ff30b221567268ce4bcb847b42c1c079e1b41e1
Michael Coughlin <michael.coughlin@ligo.org>
2020-03-05 05:03:12 -0800

    start keeping track of code

---
+++ ldasoft/eclipsing/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/home/michael.coughlin/ZTF/ldasoft/gbmcmc/src/gb_mcmc --inj /home/michael.coughlin/ZTF/ldasoft/gbmcmc/etc/sources/verification/ZTF1539.dat --known-source --no-rj --cheat --samples 128 --sim-noise --em-prior inc_prior.dat 
+
