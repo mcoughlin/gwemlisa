63edd6c15ff797c7af78966b3c9c476b03ccf367
Majoburo <majoburo@gmail.com>
2020-09-17 10:01:01 -0500

    still needs edits for last part, like the masses being hardcoded...

--- bilby_example/run_lightcurve_modeling_for_gbfisher_output.py
+++ bilby_example/run_lightcurve_modeling_for_gbfisher_output.py
@@ -39,14 +39,9 @@ binfolders = glob.glob(args.chainsdir+'/*/')
 for binary in binfolders:
     binaryname = os.path.basename(os.path.normpath(binary))
     f, fdot, col, lon, amp, incl, pol, phase = np.loadtxt(binary+binaryname+'.dat')
-#for iii, binParams in enumerate(binsParams):
-#    if iii % args.every != 0:
-#        continue
-
-#    f, fdot, col, lon, amp, incl, pol, phase = binParams
     incl = incl*180/np.pi
     b = sim.BinaryGW(f,fdot)
-    o = sim.Observation(b, numobs=1000, mean_dt=100)
+    o = sim.Observation(b, numobs=100, mean_dt=10)
     data = np.array([o.obstimes,(o.phases()-o.obstimes)*60*60*24.,o.freqs()]).T
 
 
@@ -59,8 +54,6 @@ for binary in binfolders:
         period = 2 * (1.0 / row[2]) / 86400.0
         tzero = row[0] + row[1] / 86400
 
-        filelabel = "data_{}_incl{}_errormultiplier{}".format(label, incl,
-                                                              args.error_multiplier)
         filelabel = "d_{}_errm{}".format(label, args.error_multiplier)
         simfile = "{}/{}.dat".format(args.outdir, filelabel)
         if not os.path.isfile(simfile):
@@ -73,16 +66,8 @@ for binary in binfolders:
                 cmd += " --plot"
             subprocess.run([cmd], shell=True)
 
-        jsonfile = "data_{}_incl{}_errormultiplier{}_GW-prior_result".format(label,incl,args.error_multiplier)
         jsonfile = "d_{}_errm{}_GW-prior_result".format(label,args.error_multiplier)
         chainfile = binary+'/chains/dimension_chain.dat.1'
-        #if np.isclose(incl, 90):
-        #    chainfile = '../samples/eclipsing-dimension_chain.dat.1'
-        #elif np.isclose(incl, 60):
-        #    chainfile = '../samples/noneclipsing-dimension_chain.dat.1'
-        #else:
-        #    print('Need new GW chain file... exiting.')
-        #    exit(0)
 
         postfile = "{}/{}.json".format(args.outdir, jsonfile)
         if not os.path.isfile(postfile):
@@ -108,114 +93,114 @@ for binary in binfolders:
         print('T0 true - estimated [s]: %.2f' % ((np.median(data_out[ii])-tzero)*86400))
 
 # constants (SI units)
-G = 6.67e-11 # grav constant (m^3/kg/s^2)
-msun = 1.989e30 # solar mass (kg)
-c = 299792458 # speed of light (m/s)
+    G = 6.67e-11 # grav constant (m^3/kg/s^2)
+    msun = 1.989e30 # solar mass (kg)
+    c = 299792458 # speed of light (m/s)
 
-def fdotgw(f0, mchirp):
-    return 96./5. * np.pi * (G*np.pi*mchirp)**(5/3.)/c**5*f0**(11/3.)
-def mchirp(m1, m2):
-    return (m1*m2)**(3/5.)/(m1+m2)**(1/5.)*msun
+    def fdotgw(f0, mchirp):
+        return 96./5. * np.pi * (G*np.pi*mchirp)**(5/3.)/c**5*f0**(11/3.)
+    def mchirp(m1, m2):
+        return (m1*m2)**(3/5.)/(m1+m2)**(1/5.)*msun
 
-plotDir = os.path.join(args.outdir, 'combined')
-if not os.path.isdir(plotDir):
-    os.makedirs(plotDir)
+    plotDir = os.path.join(args.outdir, binary+'combined')
+    if not os.path.isdir(plotDir):
+        os.makedirs(plotDir)
 
 # Get true values...
-m1 = 0.610
-m2 = 0.210
-true_mchirp = mchirp(m1, m2)/msun
+    m1 = 0.610
+    m2 = 0.210
+    true_mchirp = mchirp(m1, m2)/msun
 
-p0 = data[0,2]
-p0min, p0max = p0*0.9, p0*1.1
-f0 = 2./(86400*p0)
+    p0 = data[0,2]
+    p0min, p0max = p0*0.9, p0*1.1
+    f0 = 2./(86400*p0)
 
-pdot = 2.373e-11
-fdotem = pdot/p0**2
-true_fdot = fdotgw(f0, true_mchirp*msun)
+    pdot = 2.373e-11
+    fdotem = pdot/p0**2
+    true_fdot = fdotgw(f0, true_mchirp*msun)
 
 # Compare true values to what we measure...
 
-phtimes = []
-med_ress = []
-std_ress = []
+    phtimes = []
+    med_ress = []
+    std_ress = []
 
-plt.figure(figsize=(10,6))
-for ii in data_out.keys():
-    med_T0 = np.median(data_out[ii])
-    std_T0 = np.std(data_out[ii])
-    
-    res = (data_out[ii]) - data[ii,0]
-    med_res, std_res = np.median(res), np.std(res)
-    print('Residual Med: %.10f Std: %.10f' % ( med_res, std_res))
-    plt.errorbar(med_T0,(med_T0-(data[ii,0]+data[ii,1]/86400))*86400,yerr=std_res,fmt='r^')
-    plt.errorbar(med_T0,med_res*86400,yerr=std_res*86400,fmt='kx')
+    plt.figure(figsize=(10,6))
+    for ii in data_out.keys():
+        med_T0 = np.median(data_out[ii])
+        std_T0 = np.std(data_out[ii])
+        
+        res = (data_out[ii]) - data[ii,0]
+        med_res, std_res = np.median(res), np.std(res)
+        print('Residual Med: %.10f Std: %.10f' % ( med_res, std_res))
+        plt.errorbar(med_T0,(med_T0-(data[ii,0]+data[ii,1]/86400))*86400,yerr=std_res,fmt='r^')
+        plt.errorbar(med_T0,med_res*86400,yerr=std_res*86400,fmt='kx')
 
-    print(med_res)
+        print(med_res)
 
-    theory = - 1/2*(true_fdot/f0)*(med_T0*86400)**2
-    plt.plot(med_T0,theory,'go')
+        theory = - 1/2*(true_fdot/f0)*(med_T0*86400)**2
+        plt.plot(med_T0,theory,'go')
 
-    phtimes.append(med_T0*86400)
-    med_ress.append(med_res*86400)
-    std_ress.append(std_res*86400)
+        phtimes.append(med_T0*86400)
+        med_ress.append(med_res*86400)
+        std_ress.append(std_res*86400)
 
-phtimes, med_ress, std_ress = np.array(phtimes), np.array(med_ress), np.array(std_ress)
+    phtimes, med_ress, std_ress = np.array(phtimes), np.array(med_ress), np.array(std_ress)
 
-plt.ylabel("Residual [seconds]")
-plt.xlabel("$\Delta T$")
-plt.show()
-plt.savefig(os.path.join(plotDir,"residual.pdf"), bbox_inches='tight')
-plt.close()
+    plt.ylabel("Residual [seconds]")
+    plt.xlabel("$\Delta T$")
+    plt.show()
+    plt.savefig(os.path.join(plotDir,"residual.pdf"), bbox_inches='tight')
+    plt.close()
 
-def myprior(cube, ndim, nparams):
-        cube[0] = cube[0]*2.0 # chirp mass
+    def myprior(cube, ndim, nparams):
+            cube[0] = cube[0]*2.0 # chirp mass
 
-def myloglike(cube, ndim, nparams):
-    mchirp = cube[0]
+    def myloglike(cube, ndim, nparams):
+        mchirp = cube[0]
 
-    fdot = fdotgw(f0, mchirp*msun)
-    theory = - 1/2*(fdot/f0)*(phtimes)**2
+        fdot = fdotgw(f0, mchirp*msun)
+        theory = - 1/2*(fdot/f0)*(phtimes)**2
 
-    x = theory - med_ress
-    prob = ss.norm.logpdf(x, loc=0.0, scale=std_ress)
-    prob = np.sum(prob)
+        x = theory - med_ress
+        prob = ss.norm.logpdf(x, loc=0.0, scale=std_ress)
+        prob = np.sum(prob)
 
-    if np.isnan(prob):
-        prob = -np.inf
+        if np.isnan(prob):
+            prob = -np.inf
 
-    return prob
+        return prob
 
 # Estimate chirp mass based on the observations
 
-n_live_points = 1000
-evidence_tolerance = 0.5
-max_iter = 0
-title_fontsize = 26
-label_fontsize = 30
+    n_live_points = 1000
+    evidence_tolerance = 0.5
+    max_iter = 0
+    title_fontsize = 26
+    label_fontsize = 30
 
-parameters = ["chirp_mass"]
-labels = [r"$M_c$"]
-n_params = len(parameters)
+    parameters = ["chirp_mass"]
+    labels = [r"$M_c$"]
+    n_params = len(parameters)
 
-pymultinest.run(myloglike, myprior, n_params, importance_nested_sampling = False, resume = True, verbose = True, sampling_efficiency = 'parameter', n_live_points = n_live_points, outputfiles_basename='%s/2-'%plotDir, evidence_tolerance = evidence_tolerance, multimodal = False, max_iter = max_iter)
+    pymultinest.run(myloglike, myprior, n_params, importance_nested_sampling = False, resume = True, verbose = True, sampling_efficiency = 'parameter', n_live_points = n_live_points, outputfiles_basename='%s/2-'%plotDir, evidence_tolerance = evidence_tolerance, multimodal = False, max_iter = max_iter)
 
-multifile = "%s/2-post_equal_weights.dat"%plotDir
-data = np.loadtxt(multifile)
+    multifile = "%s/2-post_equal_weights.dat"%plotDir
+    data = np.loadtxt(multifile)
 
 # Show that they are consistent with the injection...
 
-fdot = fdotgw(f0, data[:,0]*msun)
-print('Estimated chirp mass: %.5e +- %.5e' % (np.median(data[:,0]), np.std(data[:,0])))
-print('Estimated fdot: %.5e +- %.5e' % (np.median(fdot), np.std(fdot)))
-print('True fdot: %.5e, chirp mass: %.5e' % (true_fdot, true_mchirp))
+    fdot = fdotgw(f0, data[:,0]*msun)
+    print('Estimated chirp mass: %.5e +- %.5e' % (np.median(data[:,0]), np.std(data[:,0])))
+    print('Estimated fdot: %.5e +- %.5e' % (np.median(fdot), np.std(fdot)))
+    print('True fdot: %.5e, chirp mass: %.5e' % (true_fdot, true_mchirp))
 
-plotName = "%s/corner.pdf"%(plotDir)
-figure = corner.corner(data[:,:-1], labels=labels,
-                       quantiles=[0.16, 0.5, 0.84],
-                       show_titles=True, title_kwargs={"fontsize": title_fontsize},
-                       label_kwargs={"fontsize": label_fontsize}, title_fmt=".3f",
-                       smooth=3)
-figure.set_size_inches(18.0,18.0)
-plt.savefig(plotName, bbox_inches='tight')
-plt.close()
+    plotName = "%s/corner.pdf"%(plotDir)
+    figure = corner.corner(data[:,:-1], labels=labels,
+                           quantiles=[0.16, 0.5, 0.84],
+                           show_titles=True, title_kwargs={"fontsize": title_fontsize},
+                           label_kwargs={"fontsize": label_fontsize}, title_fmt=".3f",
+                           smooth=3)
+    figure.set_size_inches(18.0,18.0)
+    plt.savefig(plotName, bbox_inches='tight')
+    plt.close()
