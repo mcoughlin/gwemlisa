19429a3a8cc5949536fb05e23da053c6d98cf638
joh15016@umn.edu <joh15016@umn.edu>
2021-06-30 15:01:57 -0500

    refactor large blocks of code and add support for some addional binary parameters

--- bilby_example/common.py
+++ bilby_example/common.py
@@ -1,18 +1,20 @@
 import ellc
 import bilby
 import numpy as np
+import scipy.stats as ss
+from scipy.interpolate import InterpolatedUnivariateSpline as ius
 
-Msun = 4.9169e-6  # mass of sun in s
-pc = 3.0856775807e16  # parsec in m
-c = 299792458  # m/s
-kpc = 1e3 * pc  # kiloparsec in m
-day = 86400  # s
+# constants (SI units)
+G = 6.67e-11       # grav constant (m^3/kg/s^2)
+msun = 1.989e30    # solar mass (kg)
+rsun = 6.957e8     # solar radius (m)
+c = 299792458      # speed of light (m/s)
 
 
 # Fixed injection parameters
 DEFAULT_INJECTION_PARAMETERS = dict(
-    radius_1=0.125, radius_2=0.3, sbratio=1 / 15.0, q=0.4, heat_2=5,
-    ldc_1=0.2, ldc_2=0.4548, gdc_2=0.61, f_c=0, f_s=0, t_exp=3.0 / 86400)
+    radius_1=0.125, radius_2=0.3, sbratio=1/15., q=0.4, heat_2=5,
+    ldc_1=0.2, ldc_2=0.4548, gdc_2=0.61, f_c=0, f_s=0, t_exp=3./(60*60*24.))
 
 
 class GaussianLikelihood(bilby.core.likelihood.Analytical1DLikelihood):
@@ -46,13 +48,12 @@ class GaussianLikelihood(bilby.core.likelihood.Analytical1DLikelihood):
             self.parameters['sigma'] = None
 
     def log_likelihood(self):
-        log_l = np.sum(- (self.residual / self.sigma)**2 / 2 - 
-                np.log(2 * np.pi * self.sigma**2) / 2)
+        #log_l = np.sum(- (self.residual / self.sigma)**2 / 2 - np.log(2 * np.pi * self.sigma**2) / 2)
+        log_l = np.sum(ss.norm.logpdf(self.residual, loc=0.0, scale=self.sigma))
         return np.nan_to_num(log_l)
 
     def __repr__(self):
-        return self.__class__.__name__ + '(x={}, y={}, func={}, sigma={})' \
-            .format(self.x, self.y, self.func.__name__, self.sigma)
+        return self.__class__.__name__+f'(x={self.x}, y={self.y}, func={self.func.__name__}, sigma={self.sigma})'
 
     @property
     def sigma(self):
@@ -79,17 +80,18 @@ class GaussianLikelihood(bilby.core.likelihood.Analytical1DLikelihood):
 def basic_model(t_obs, radius_1, radius_2, sbratio, t_zero, q, period,
                 heat_2, scale_factor, ldc_1, ldc_2, gdc_2, f_c, f_s, t_exp,
                 incl):
-    """ A function which returns model values at times t for parameters pars
-
-    input:
-        t    a 1D array with times
-        pars a 1D array with parameter values; r1,r2,J,i,t0,p
-
-    output:
-        m    a 1D array with model values at times t
-
     """
-        
+    ------ TODO: Create Updated Description -----
+
+    Function which returns model values at times t for parameters pars
+
+    INPUTS:
+        t = 1D array with times
+        pars = 1D array with parameter values; r1,r2,J,i,t0,p
+    OUTPUT:
+        m = 1D array with model values at times t
+    """
+
     grid = "very_sparse"
     exact_grav = False
     verbose = 0
@@ -121,59 +123,203 @@ def basic_model(t_obs, radius_1, radius_2, sbratio, t_zero, q, period,
         m *= scale_factor
     except Exception as e:
         return t_obs * 10**99
-
     return m
+
 
 def basic_model_pdot(t_obs, radius_1, radius_2, sbratio, incl, t_zero,
                      q, period, heat_2, scale_factor, ldc_1, ldc_2, gdc_2,
                      f_c, f_s, t_exp, Pdot):
-
     phases = pdot_phasefold(t_obs,P=period,Pdot=Pdot*(60*60*24.)**2,t0=0)
-  
     tmods, fluxes = [], []
     for ii in range(len(t_obs)):
         P_new = period - Pdot*t_obs[ii]*(60*60*24.)**2
         flux = basic_model(phases*P_new, radius_1, radius_2, sbratio, incl,
                            t_zero, q, P_new, heat_2, scale_factor,
                            ldc_1, ldc_2, gdc_2, f_c, f_s, t_exp)
-
         tmod = np.mod(t_obs[ii],P_new)
         tmods.append(tmod)
         phot = np.interp(tmod,t_obs,flux,period=P_new)
         fluxes.append(phot)
-       
     return fluxes
 
 
 def pdot_phasefold(times, P, Pdot, t0=0):
     """
     @author: kburdge
-       
+
     Function which returns phases corresponding to timestamps in a lightcurve 
     given a period P, period derivative Pdot, and reference epoch t0
-    
+
     If no reference epoch is supplied, reference epoch is set to earliest time in lightcurve
-    
+
     INPUTS:
-            times = time array
-            
-            P = starting period
-            
-            Pdot = rate of change of period in units of time/time
-            
-            t0 = start time
-            
+        times = time array
+        P = starting period
+        Pdot = rate of change of period in units of time/time
+        t0 = start time
     OUTPUTS:
-            phases = phases for given time array, period, and Pdot
-    
+        phases = phases for given time array, period, and Pdot
     """
-    
-    if t0==0:
-            times=times-np.min(times)
+
+    if t0 == 0:
+        times = times - np.min(times)
     else:
-            times=times-t0
-    
-    phases=((times-1/2*Pdot/P*(times)**2) % P)/P
-    
+        times = times - t0
+    phases = ((times-1/2*Pdot/P*(times)**2) % P)/P
     return phases
 
+
+class BinaryGW:
+
+    def __init__(self,
+        f0,
+        fdot,
+        incl,
+        q
+    ):
+        """
+        A class for representing binaries with GW parameters in mind
+
+        Assumes orbits are roughly circular (e = 0)
+
+        Parameters
+        ----------
+        f0: float
+            starting frequency [hertz]
+        fdot: float
+            starting rate of change of frequency [hertz/sec]
+        incl: float
+            inclination with respect to observer [degrees]
+        q: float
+            mass ratio (m2/m1)
+
+        Properties
+        ----------
+        p0: float
+            starting period [days]
+        pdot: float
+            starting absolute rate of change of period
+        mchirp: float
+            chirp mass [Solar Masses]
+        tcoal: float
+            time to coalescence [seconds]
+        m1: float
+            mass of body 1 (primary) [Solar Masses]
+        m2: float
+            mass of body 2 (secondary) [Solar Masses]
+        a: float
+            mean binary separation (semi-major axis) [Solar Radii]
+        R1: float
+            radius of body 1 scaled by separation
+        R2: float
+            radius of body 2 scaled by separation
+        K1: float
+            line-of-sight radial velocity amplitude of body 1 [km/s]
+        K2: float
+            line-of-sight radial velocity amplitude of body 2 [km/s]
+        """
+
+        self.f0 = f0
+        self.fdot = fdot
+        self.incl = incl
+        self.q = q
+        self.eta = self.q/(1+self.q)**2
+        self.root = np.sqrt(0.25-self.eta)
+        self.fraction = (0.5+self.root) / (0.5-self.root)
+
+        # Generate white dwarf mass-radius relation spline curve
+        wd_eof = np.loadtxt("wd_mass_radius.dat", delimiter=",")
+        self._spl = ius(wd_eof[:,0], wd_eof[:,1])
+
+    @property
+    def p0(self):
+        return 2./self.f0/(60*60*24.)
+    @property
+    def pdot(self):
+        return 2.*self.fdot/self.f0**2
+    @property
+    def mchirp(self):
+        return c**3/G*(5/96.*np.pi**(-8/3.)*self.f0**(-11/3.)*self.fdot)**(3/5.)/msun
+    @property
+    def tcoal(self):
+        return 5./256. * (np.pi*self.f0)**(-8/3) * (G*self.mchirp/c**3)**(-5/3)
+    @property
+    def m1(self):
+        return self.mchirp * np.power(1+1/self.fraction,0.2) / np.power(1/self.fraction,0.6)
+    @property
+    def m2(self):
+        return self.mchirp * np.power(1+self.fraction,0.2) / np.power(self.fraction,0.6)
+    @property
+    def a(self):
+        return (G*(self.m1+self.m2)*msun/((np.pi*self.f0)**2))**(1/3)/rsun
+    @property
+    def r1(self):
+        return self._spl(self.m1)/self.a
+    @property
+    def r2(self):
+        return self._spl(self.m2)/self.a
+    @property
+    def k1(self):
+        return 2*np.pi*self.f0*(self.a*rsun)*np.sin(np.radians(self.incl))/(1+1/self.q)/1000.
+    @property
+    def k2(self):
+        return 2*np.pi*self.f0*(self.a*rsun)*np.sin(np.radians(self.incl))/(1+self.q)/1000.
+
+
+class Observation:
+    def __init__(self,
+        binary,
+        t0 = 0.,
+        numobs = 25,
+        mean_dt = 120.,
+        std_dt = 5.
+    ):
+        """
+        A class for representing EM binary observations.
+
+        Parameters
+        ----------
+        binary: BinaryGW
+            binary object being observed
+        t0: float
+            starting time of observations [days]
+        numobs: int
+            number of observations
+        mean_dt: float
+            average dt between each observation time [days]
+        std_dt: float
+            standard deviation of the observation times [days]
+
+        Properties
+        ----------
+        obstimes: float array
+            array with observation times [days].
+        freqs: float array
+            frequencies at the times of observation [hertz]
+        phases: float array
+            times of eclipses [days]
+        """
+
+        self.binary = binary
+        self.t0 = t0
+        self.numobs = numobs
+        self.mean_dt = mean_dt
+        self.std_dt = std_dt
+        t = np.full(self.numobs,self.t0)
+        for i in range(1,len(t)):
+            t[i] = t[i-1] + np.abs(np.random.normal(self.mean_dt,self.std_dt,1))
+        self._t = t
+
+    @property
+    def obstimes(self):
+        return self._t
+
+    @property
+    def freqs(self):
+        tau = self.binary.tcoal - self.obstimes*(60*60*24.)
+        return 1/np.pi*(5/256./tau)**(3/8.)*(G*self.binary.mchirp/c**3)**(-5/8.)
+
+    @property
+    def phases(self):
+        phtimes = (self.obstimes - self.t0)*(60*60*24.)
+        return (phtimes - 1/2*(self.binary.fdot/self.binary.f0)*phtimes**2)/(60*60*24.)
--- bilby_example/run_lightcurve_modeling.py
+++ bilby_example/run_lightcurve_modeling.py
@@ -6,45 +6,19 @@ import subprocess
 import numpy as np
 import pymultinest
 import scipy.stats as ss
-from scipy.interpolate import InterpolatedUnivariateSpline as ius
-
-import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.use('Agg')
-font = {'family':'normal','size':18}
-matplotlib.rc('font', **font)
 import corner
 
-import simulate_binaryobs_gwem as sim
-from common import basic_model_pdot, pdot_phasefold, DEFAULT_INJECTION_PARAMETERS
+from common import basic_model_pdot, pdot_phasefold
+from common import DEFAULT_INJECTION_PARAMETERS, BinaryGW, Observation
 
 home = os.path.expanduser("~")
 
-# constants (SI units)
-G = 6.67e-11 # grav constant (m^3/kg/s^2)
-msun = 1.989e30 # solar mass (kg)
-c = 299792458 # speed of light (m/s)
+# Constants (SI units)
+G = 6.67e-11       # gravitational constant (m^3/kg/s^2)
+msun = 1.989e30    # solar mass (kg)
+c = 299792458      # speed of light (m/s)
 
-
-def greedy_kde_areas_2d(pts):
-    pts = np.random.permutation(pts)
-    mu = np.mean(pts, axis=0)
-    cov = np.cov(pts, rowvar=0)
-
-    L = np.linalg.cholesky(cov)
-    detL = L[0,0]*L[1,1]
-    pts = np.linalg.solve(L, (pts - mu).T).T
-
-    Npts = pts.shape[0]
-    kde_pts = pts[:int(Npts/2), :]
-    den_pts = pts[int(Npts/2):, :]
-
-    kde = ss.gaussian_kde(kde_pts.T)
-    kdedir = {}
-    kdedir["kde"] = kde
-    kdedir["mu"] = mu
-    kdedir["L"] = L
-    return kdedir
 
 def greedy_kde_areas_1d(pts):
     pts = np.random.permutation(pts)
@@ -60,15 +34,6 @@ def greedy_kde_areas_1d(pts):
     kdedir["mu"] = mu
     return kdedir
 
-def kde_eval(kdedir,truth):
-    kde = kdedir["kde"]
-    mu = kdedir["mu"]
-    L = kdedir["L"]
-
-    truth = np.linalg.solve(L, truth-mu)
-    td = kde(truth)
-    return td
-
 def kde_eval_single(kdedir,truth):
     kde = kdedir["kde"]
     mu = kdedir["mu"]
@@ -82,8 +47,8 @@ def fdotgw(f0, mchirp):
 # Set up the argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("--outdir", default="out-gwprior", help="Path to output directory")
-parser.add_argument("-m", "--error-multiplier", default=0.1, type=float)
-parser.add_argument("--every", default=1, type=int, help="Downsample of phase_freq.dat")
+parser.add_argument("--error-multiplier", default=0.1, type=float, help="Simulated lightcurve error multiplier")
+parser.add_argument("--every", default=1, type=int, help="Downsample of observations")
 parser.add_argument("--chainsdir", default=f"{home}/gwemlisa/data/results", help="Path to binaries directory")
 parser.add_argument("--binary", type=int, help="Binary indexing number")
 parser.add_argument("--numobs", default=25, type=int, help="Number of obsevations")
@@ -96,81 +61,61 @@ parser.add_argument("--test", action="store_true", help="Enable temporary test p
 args = parser.parse_args()
 
 
-# Read in and compute true binary parameters
-data_out = {}
-data_out["t0"] = {}
-data_out["inc"] = {}
-
 if args.test:
+    # Parameters useful for testing purposes
     binaryname = "binary001"
     f, fdot, col, lon, amp, incl, pol, phase = \
     0.002820266, 2.10334e-17, 1.40157, 4.66848, 2.07938e-23, 1.86512, 1.33296, 0.634247
-    np.random.seed(0)
+    np.random.seed(314)
 else:
+    # Read-in true parameter values from chain directory files
     binary = os.path.join(args.chainsdir, os.listdir(args.chainsdir)[args.binary-1])
     binaryname = os.path.basename(os.path.normpath(binary))
     f, fdot, col, lon, amp, incl, pol, phase = np.loadtxt(os.path.join(binary, f"{binaryname}.dat"))
     np.random.seed(args.binary)
 
-wd_eof = np.loadtxt("wd_mass_radius.dat", delimiter=",")
-mass,radius=wd_eof[:,0],wd_eof[:,1]
-spl = ius(mass,radius)
-incl = 90 - np.abs(np.degrees(incl) - 90)
-massratio = np.random.rand()*0.5 + 0.5
+incl = 90 - np.abs(np.degrees(incl) - 90)    # convert true incl to deg and map between 0-90 deg
+massratio = np.random.rand()*0.5 + 0.5       # generate "true" mass-ratio between 0.5-1 (m2/m1)
 
-# Generate simulated binary
-b = sim.BinaryGW(f,fdot,1/massratio)
-sep = b.r1+b.r2
-mass1 = b.m1
-mass2 = b.m2
-rad1 = spl(mass1)*6.957e8/sep
-rad2 = spl(mass2)*6.957e8/sep
+# Generate simulated binary white dwarf system
+b = BinaryGW(f, fdot, incl, massratio)
 
-# Generate observations from simulated binary
-o = sim.Observation(b, numobs=args.numobs, mean_dt=args.mean_dt, std_dt=args.std_dt)
-data = np.array([o.obstimes,(o.phases()-o.obstimes)*60*60*24.,o.freqs()]).T
-print(f'Period (days): {2 * (1.0 / b.f0) / 86400.0:.10f}')
+# Generate observations based on simulated binary
+o = Observation(b, numobs=args.numobs, mean_dt=args.mean_dt, std_dt=args.std_dt)
+data = np.array([o.obstimes,(o.phases-o.obstimes)*(60*60*24.),o.freqs]).T
+print(f'True period (days): {b.p0:.10f}')
 
 
-# Periodfind algorithm
+# Periodfind Algorithm
 if args.periodfind:
-    period = 2 * (1.0 / b.f0) / 86400.0
     # Set up the full set of injection_parameters
     injection_parameters = DEFAULT_INJECTION_PARAMETERS
     injection_parameters["incl"] = 90
-    injection_parameters["period"] = period
+    injection_parameters["period"] = b.p0
     injection_parameters["t_zero"] = o.obstimes[0]
     injection_parameters["scale_factor"] = 1
-    injection_parameters["q"] = massratio
-    injection_parameters["radius_1"] = rad1
-    injection_parameters["radius_2"] = rad2
+    injection_parameters["q"] = b.q
+    injection_parameters["radius_1"] = b.r1
+    injection_parameters["radius_2"] = b.r2
     injection_parameters["Pdot"] = b.pdot
-
-    mean_dt, std_dt = 3.0, 0.5
-    numobs = 1000
     
-    t = np.zeros(numobs)
-    t[0] = o.obstimes[0]
-    for i in range(len(t)):
-        if i != 0:
-            t[i] += t[i-1] + np.abs(np.random.normal(mean_dt,std_dt))
-    t = t - np.min(t)
+    # Generate list of observation times
+    t_obs = Observation(b, t0=o.obstimes[0], numobs=1000, mean_dt=3.0, std_dt=0.5).obstimes
 
     # Evaluate the injection data
-    lc = basic_model_pdot(t, **injection_parameters)
+    lc = basic_model_pdot(t_obs, **injection_parameters)
 
-    baseline = np.max(t)-np.min(t)
+    baseline = np.max(t_obs)-np.min(t_obs)
     fmin, fmax = 2/baseline, 480
     samples_per_peak = 10.0
     df = 1./(samples_per_peak * baseline)
-    fmin, fmax = 1/period - 100*df, 1/period + 100*df
-    nf = int(np.ceil((fmax - fmin) / df))
-    freqs = fmin + df * np.arange(nf)
+    fmin, fmax = 1/b.p0 - 100*df, 1/b.p0 + 100*df
+    freqs = fmin + df * np.arange(int(np.ceil((fmax - fmin) / df)))
     periods = np.sort((1/freqs).astype(np.float32))
-    pdots_to_test = np.array([0, b.pdot*(60*60*24.)**2]).astype(np.float32)
+    pdots_to_test = np.array([0, b.pdot]).astype(np.float32)
 
     lc = (lc - np.min(lc))/(np.max(lc)-np.min(lc))
-    time_stack = [t.astype(np.float32)]
+    time_stack = [t_obs.astype(np.float32)]
     mag_stack = [lc.astype(np.float32)] 
 
     from periodfind.aov import AOV
@@ -180,7 +125,7 @@ if args.periodfind:
     dataslice = data_out[0].data[:,1]
 
     low_side, high_side = 0.0, 0.0
-    jj = np.argmin(np.abs(period - periods))
+    jj = np.argmin(np.abs(b.p0 - periods))
     aovpeak = dataslice[jj]
     ii = jj + 0
     while high_side == 0.0:
@@ -197,7 +142,7 @@ if args.periodfind:
     
     period = periods[jj]
     err = np.mean([periods[jj]-low_side, high_side-periods[jj]])/periods[jj]
-    print(f'Period: {period:.10f}')
+    print(f'Estimated period: {period:.10f}')
     print(f'Average error bar: {err:.10f}')
 
 
@@ -211,9 +156,9 @@ for ii, row in enumerate(data):
 
     # Simulate binary lightcurve from true parameters
     label = f"{binaryname}row{ii}"
-    tzero = row[0] + row[1] / 86400
+    tzero = row[0] + row[1] / (60*60*24.)
     if not args.periodfind:
-        period = 2 * (1.0 / row[2]) / 86400.0
+        period = (2.0 / row[2]) / (60*60*24.)
 
     filelabel = f"data_{label}_incl{incl}"  
     simfile = f"{args.outdir}/{binaryname}/{filelabel}.dat"
@@ -221,8 +166,8 @@ for ii, row in enumerate(data):
         cmd = (
             f"python simulate_lightcurve.py --outdir {args.outdir}/{binaryname} "
             f"--plot --label {label} --err-lightcurve ../data/JulyChimeraBJD.csv "
-            f"--error-multiplier {args.error_multiplier} --t-zero {tzero} --incl {incl} "
-            f"--period {period} --massratio {massratio} --radius1 {rad1} --radius2 {rad2}"
+            f"--error-multiplier {args.error_multiplier} --t-zero {tzero} --incl {b.incl} "
+            f"--period {period} --massratio {b.q} --radius1 {b.r1} --radius2 {b.r2}"
         )
         subprocess.run([cmd], shell=True)
 
@@ -231,11 +176,13 @@ for ii, row in enumerate(data):
     else:
         filelabel += f"_EM-prior_result"
     postfile = f"{args.outdir}/{binaryname}/{filelabel}.json"
+
+    # Recover parameters from the simulated lightcurve using either GW or EM priors
     if not os.path.isfile(postfile):
         cmd = (
             f"python analyse_lightcurve.py --outdir {args.outdir}/{binaryname} "
-            f"--lightcurve {simfile} --nlive {args.nlive} --t-zero {tzero} --incl {incl} "
-            f"--period {period} --massratio {massratio} --radius1 {rad1} --radius2 {rad2}"
+            f"--lightcurve {simfile} --nlive {args.nlive} --t-zero {tzero} --incl {b.incl} "
+            f"--period {period} --massratio {b.q} --radius1 {b.r1} --radius2 {b.r2}"
         )
         if args.periodfind:
             cmd += f" --period-err {err}"
@@ -246,163 +193,116 @@ for ii, row in enumerate(data):
 
     with open(postfile) as json_file:
         post_out = json.load(json_file)
-
-    idx = post_out["parameter_labels"].index("$t_0$")
-    idx2 = post_out["parameter_labels"].index("$\\iota$")
     
     t_0, inc = [], []
     for row in post_out["samples"]["content"]:
-        t_0.append(row[idx])
-        inc.append(row[idx2])
+        t_0.append(row[post_out["parameter_labels"].index("$t_0$")])
+        inc.append(row[post_out["parameter_labels"].index("$\\iota$")])
     data_out["t0"][ii] = np.array(t_0)
     data_out["inc"][ii] = np.array(inc)
-
-    print('')
-    print(f'T0 true: {tzero * 86400:.10f}')
-    print(f'T0 estimated: {np.median(data_out["t0"][ii]*86400):.10f} +- {np.std(data_out["t0"][ii]*86400):.10f}')
-    print(f'T0 true - estimated [s]: {(np.median(data_out["t0"][ii])-tzero)*86400:.2f}')
 
 
 # Prior and likelihood functions for inclination recovery
 def myprior(cube, ndim, nparams):
-    cube[0] = cube[0]*180.0
+    cube[0] = cube[0]*90.0
 
 def myloglike(cube, ndim, nparams):
-    inc = cube[0]
     prob = 0
     for kdedir in kdedirs: 
-        prob = prob + np.log(kde_eval_single(kdedir,[inc])[0])
+        prob = prob + np.log(kde_eval_single(kdedir,[cube[0]])[0])
     if np.isnan(prob):
         prob = -np.inf
     return prob
 
-# Estimate the inclination based on the observation and plot inclination distributions
-plotDir = os.path.join(args.outdir, binaryname, 'inc')
+# Create "inc" directory for storing plots and data
+plotDir = os.path.join(args.outdir, binaryname, "inc")
 if not os.path.isdir(plotDir):
     os.makedirs(plotDir)
+
+# Some mysterious algorithms
 kdedirs = []
 for ii in data_out["inc"].keys():
     kdedirs.append(greedy_kde_areas_1d(data_out["inc"][ii]))
 
-n_live_points = 1000
-evidence_tolerance = 0.5
-max_iter = 0
-title_fontsize = 26
-label_fontsize = 30
+# Estimate the inclination based on the observations
+n_params = len(["inclination"])
+pymultinest.run(myloglike, myprior, n_params, resume=True, verbose=True,
+        importance_nested_sampling=False, n_live_points=1000,
+        multimodal=False, outputfiles_basename=f"{plotDir}/2-")
+multidata = np.loadtxt(f"{plotDir}/2-post_equal_weights.dat")
 
-parameters = ["inclination"]
-labels = [r"$\iota$"]
-n_params = len(parameters)
-
-pymultinest.run(myloglike, myprior, n_params, importance_nested_sampling = False, resume = True, 
-        verbose = True, sampling_efficiency = 'parameter', n_live_points = n_live_points, 
-        outputfiles_basename=f'{plotDir}/2-', evidence_tolerance = evidence_tolerance, 
-        multimodal = False, max_iter = max_iter)
-multifile = f"{plotDir}/2-post_equal_weights.dat"
-multidata = np.loadtxt(multifile)
-
-plotName = f"{plotDir}/incl_corner.png"
-figure = corner.corner(multidata[:,:-1], labels=labels, show_titles=True, truths=[incl], 
-        quantiles=[0.16, 0.5, 0.84], title_kwargs={"fontsize": title_fontsize}, 
-        label_kwargs={"fontsize": label_fontsize}, title_fmt=".3f", smooth=3)
+# Plot inclination distribution
+figure = corner.corner(multidata[:,:-1], labels=[r"$\iota$"], show_titles=True,
+        truths=[b.incl], quantiles=[0.16, 0.5, 0.84], title_fmt='.3f', smooth=0.9,
+        title_kwargs=dict(fontsize=26), label_kwargs=dict(fontsize=30))
 figure.set_size_inches(12.0,12.0)
-plt.savefig(plotName, bbox_inches='tight')
+plt.savefig(f"{plotDir}/incl_corner.png", bbox_inches='tight')
 plt.close()
 
 
-# Compute period residuals and plot against time
-plotDir = os.path.join(args.outdir, binaryname, 'fdot')
+# Create "fdot" directory for storing plots and data
+plotDir = os.path.join(args.outdir, binaryname, "fdot")
 if not os.path.isdir(plotDir):
     os.makedirs(plotDir)
 
-true_mchirp = b.mchirp/msun
-true_fdot = b.fdot
-p0 = b.p0
-f0 = b.f0
-p0min, p0max = p0*0.9, p0*1.1
-
-phtimes = []
-med_ress = []
-std_ress = []
-
-plt.figure(figsize=(10,6))
+# Compute and store period residuals
+phtimes, med_ress, std_ress = [], [], []
 for ii in data_out["t0"].keys():
     med_T0 = np.median(data_out["t0"][ii])
-    std_T0 = np.std(data_out["t0"][ii])
-    
-    res = (data_out["t0"][ii]) - data[ii,0]
+    res = (data_out["t0"][ii]-data[ii,0])*(60*60*24.)
     med_res, std_res = np.median(res), np.std(res)
-    print(f'Residual Med: {med_res:.10f} Std: {std_res:.10f}')
-    plt.errorbar(med_T0,(med_T0-(data[ii,0]+data[ii,1]/86400))*86400,yerr=std_res,fmt='r^')
-    plt.errorbar(med_T0,med_res*86400,yerr=std_res*86400,fmt='kx')
-
-    theory = - 1/2*(true_fdot/f0)*(med_T0*86400)**2
-    plt.plot(med_T0,theory,'go')
-
-    phtimes.append(med_T0*86400)
-    med_ress.append(med_res*86400)
-    std_ress.append(std_res*86400)
-
+    phtimes.append(med_T0)
+    med_ress.append(med_res)
+    std_ress.append(std_res)
 phtimes, med_ress, std_ress = np.array(phtimes), np.array(med_ress), np.array(std_ress)
 
-plt.ylabel("Residual [seconds]")
+# Plot period residuals against time
+plt.figure(figsize=(10,6))
+plt.plot(phtimes,-1/2*(b.fdot/b.f0)*(phtimes*(60*60*24.))**2,'go',label="theoretical w/ fdot")
+plt.plot(phtimes,(phtimes-data[:,0])*(60*60*24.)-data[:,1],'r^',label="theoretical w/o fdot")
+plt.errorbar(phtimes,med_ress,yerr=std_ress,fmt='kx',label="observed")
 plt.xlabel("$\Delta T$")
+plt.ylabel("Residual [seconds]")
 plt.legend()
-plt.show()
-plt.savefig(os.path.join(plotDir,"residual.png"), bbox_inches='tight')
+plt.savefig(f"{plotDir}/residual.png", bbox_inches='tight')
 plt.close()
 
 
 # Prior and likelihood functions for chirp mass recovery
 def myprior(cube, ndim, nparams):
-    cube[0] = cube[0]*2.0 # chirp mass
+    cube[0] = cube[0]*1.22    # largest possible chirp mass for WDB
 
 def myloglike(cube, ndim, nparams):
     mchirp = cube[0]
-    fdot = fdotgw(f0, mchirp*msun)
-    theory = - 1/2*(fdot/f0)*(phtimes)**2
-    
-    x = theory - med_ress
-    prob = ss.norm.logpdf(x, loc=0.0, scale=std_ress)
-    prob = np.sum(prob)
+    fdot = fdotgw(b.f0, mchirp*msun)
+    theory = -1/2*(fdot/b.f0)*(phtimes*(60*60*24.))**2
+    prob = np.sum(ss.norm.logpdf(theory, loc=med_ress, scale=std_ress))
     if np.isnan(prob):
         prob = -np.inf
     return prob
 
 # Estimate chirp mass based on the observations
-n_live_points = 1000
-evidence_tolerance = 0.5
-max_iter = 0
-title_fontsize = 26
-label_fontsize = 30
-parameters = ["chirp_mass"]
-labels = [r"$M_c$",r"$\log_{10} \dot{f}$"]
-n_params = len(parameters)
+n_params = len(["chirp_mass"])
+pymultinest.run(myloglike, myprior, n_params, resume=True, verbose=True,
+        importance_nested_sampling=False, n_live_points=1000,
+        multimodal=False, outputfiles_basename=f"{plotDir}/2-")
+multidata = np.loadtxt(f"{plotDir}/2-post_equal_weights.dat")
 
-pymultinest.run(myloglike, myprior, n_params, importance_nested_sampling = False, resume = True, 
-        verbose = True, sampling_efficiency = 'parameter', n_live_points = n_live_points, 
-        outputfiles_basename=f'{plotDir}/2-', evidence_tolerance = evidence_tolerance, 
-        multimodal = False, max_iter = max_iter)
-multifile = f"{plotDir}/2-post_equal_weights.dat"
-multidata = np.loadtxt(multifile)
-
-# Show that chirp mass and fdot are consistent with the injection and produce corner plot of chirp mass against log_fdot
-fdot = fdotgw(f0, multidata[:,0]*msun)
+# Show that chirp mass and fdot are consistent with the injection
+fdot = fdotgw(b.f0, multidata[:,0]*msun)
 fdot_log10 = np.array([np.log10(x) for x in fdot])
 multidata = np.vstack((multidata[:,0], fdot_log10)).T
 
-print(f'Estimated chirp mass: {np.median(data[:,0]):.5e} +- {np.std(data[:,0]):.5e}')
-print(f'Estimated fdot: {np.median(fdot):.5e} +- {np.std(fdot):.5e}')
-print(f'True fdot: {true_fdot:.5e}, chirp mass: {true_mchirp:.5e}')
-
-plotName = f"{plotDir}/mchirp_corner.png"
-figure = corner.corner(multidata, labels=labels, show_titles=True, quantiles=[0.16, 0.5, 0.84],
-        truths=[true_mchirp,np.log10(true_fdot)], title_kwargs={"fontsize": title_fontsize}, 
-        label_kwargs={"fontsize": label_fontsize}, title_fmt=".3f", smooth=3)
+# Produce corner plot of chirp mass against log_fdot
+figure = corner.corner(multidata, labels=[r"$M_c$",r"$\log_{10} \dot{f}$"], show_titles=True,
+        truths=[b.mchirp,np.log10(b.fdot)], quantiles=[0.16, 0.5, 0.84], title_fmt='.3f',
+        smooth=0.9, title_kwargs=dict(fontsize=26), label_kwargs=dict(fontsize=30))
 figure.set_size_inches(12.0,12.0)
-plt.savefig(plotName, bbox_inches='tight')
+plt.savefig(f"{plotDir}/mchirp_corner.png", bbox_inches='tight')
 plt.close()
 
-fid = open(os.path.join(plotDir,'params.dat'),'w')
-fid.write(f'{mass1:.10f} {mass2:.10f} {rad1:.10f} {rad2:.10f}\n')
-fid.close()
+# Show that chirp mass and fdot are consistent with the injection
+print(f'True chirp mass: {b.mchirp:.5e}')
+print(f'Estimated chirp mass: {np.median(multidata[:,0]):.5e} +/- {np.std(multidata[:,0]):.5e}')
+print(f'True fdot: {b.fdot:.5e}')
+print(f'Estimated fdot: {np.median(fdot):.5e} +/- {np.std(fdot):.5e}')
