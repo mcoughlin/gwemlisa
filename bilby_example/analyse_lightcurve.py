Fixed periodfind bug and changed base units of time to seconds

bilby_example/analyse_lightcurve.py
@@ -16,11 +16,11 @@ parser.add_argument("--lightcurve", help="path to the lightcurve file")
 parser.add_argument("--nth-in", default=10, type=int,
         help="read every nth line of the lightcurve file")
 parser.add_argument("--gw-chain", help="chain file for computing GW priors")
-parser.add_argument("--period", type=float, help="period [days]")
+parser.add_argument("--period", type=float, help="period [s]")
 parser.add_argument("--period-err", default=1e-5, type=float,
-        help="period uncertainty [days]")
-parser.add_argument("--pdot", type=float, help="time rate of change of period")
-parser.add_argument("--t-zero", type=float, help="t-zero [days]")
+        help="period uncertainty [s]")
+parser.add_argument("--pdot", type=float, help="time rate of change of period [s/s]")
+parser.add_argument("--t-zero", type=float, help="t-zero [s]")
 parser.add_argument("--incl", type=float, help="inclination [deg])")
 parser.add_argument("--massratio", type=float, help="mass ratio (m2/m1)")
 parser.add_argument("--radius", type=float, nargs='+', help="radii (scaled)")
@@ -43,7 +43,7 @@ if not Path(args.outdir).is_dir():
 data = np.genfromtxt(Path(args.lightcurve), names=True)[::args.nth_in]
 
 # Set up the likelihood function
-likelihood = GaussianLikelihood(data['MJD'], data['flux'], basic_model, data['fluxerr'])
+likelihood = GaussianLikelihood(data['time'], data['flux'], basic_model, data['fluxerr'])
 
 # Set up the full set of injection parameters
 injection = DEFAULT_INJECTION_PARAMETERS
@@ -65,7 +65,7 @@ injection['heat_2'] = args.heat[1]
 priors = bilby.core.prior.PriorDict()
 priors.update({key: val for key, val in injection.items() if isinstance(val, (int, float))})
 priors['t_zero'] = Uniform(args.t_zero - args.period/2, args.t_zero + args.period/2,
-        "t_zero", latex_label="$t_0$", unit="days")
+        "t_zero", latex_label="$t_0$", unit="s")
 priors['q'] = Uniform(0.15, 1, "massratio", latex_label="q")
 priors['radius_1'] = Uniform(0, 1, "radius_1", latex_label="$r_1$")
 priors['radius_2'] = Uniform(0, 1, "radius_2", latex_label="$r_2$")
@@ -81,16 +81,16 @@ if args.gw_chain:
     # Set up GW priors for inclination and period
     data_out = np.loadtxt(Path(args.gw_chain))
     tau = 3/8 * args.period/args.pdot
-    period_prior_vals = (2/data_out[:, 0] / (60*60*24)) * (1 - args.t_zero/tau)**(3/8)
-    priors['period'] = KDE_Prior(period_prior_vals, "period", latex_label="P", unit="days")
+    period_prior_vals = (2/data_out[:, 0]) * (1 - args.t_zero/tau)**(3/8)
+    priors['period'] = KDE_Prior(period_prior_vals, "period", latex_label="P", unit="s")
     incl_prior_vals = 90 - np.abs(np.degrees(np.arccos(data_out[:, 5])) - 90)
     priors['incl'] = KDE_Prior(incl_prior_vals, "incl", latex_label=r"$\iota$",
             unit="$^\circ$", minimum=0, maximum=90)
     label += '_GW-prior'
 else:
     # Set up EM priors for inclination and period
-    priors['period'] = Normal(args.period, args.period_err, "period", latex_label="$P_0$", unit="days")
-    priors['incl'] = Uniform_Cosine_Prior(0, 90, "incl", latex_label=r"$\iota$", unit="deg")
+    priors['period'] = Normal(args.period, args.period_err, "period", latex_label="P", unit="s")
+    priors['incl'] = Uniform_Cosine_Prior(0, 90, "incl", latex_label=r"$\iota$", unit="$^\circ$")
     label += '_EM-prior'
 
 with warnings.catch_warnings():

bilby_example/common.py
@@ -14,10 +14,7 @@ MSUN = 1.989e30    # solar mass (kg)
 
 
 # Default injection values
-DEFAULT_INJECTION_PARAMETERS = dict(
-    ldc_1=0.2, ldc_2=0.4548, gdc_1=None, gdc_2=0.61, scale_factor=1, incl=90,
-    f_c=0, f_s=0, sbratio=0.25, heat_1=None, heat_2=5, t_exp=3/(60*60*24)
-)
+DEFAULT_INJECTION_PARAMETERS = dict(scale_factor=1, incl=90, f_c=0, f_s=0, t_exp=3)
 
 
 def basic_model(t_obs, t_zero, period, q, incl, radius_1, radius_2, ldc_1, ldc_2,
@@ -28,13 +25,13 @@ def basic_model(t_obs, t_zero, period, q, incl, radius_1, radius_2, ldc_1, ldc_2
     Parameters
     ==========
     t_obs : array_like
-        array of observation times [days]
+        array of observation times [s]
 
     t_zero : float
-        starting time of observations [days]
+        starting time of observations [s]
 
     period : float
-        starting orbital period [days]
+        starting orbital period [s]
 
     q : float
         mass ratio (m2/m1)
@@ -76,7 +73,7 @@ def basic_model(t_obs, t_zero, period, q, incl, radius_1, radius_2, ldc_1, ldc_2
         coefficient for simplified reflection model
 
     t_exp : float
-        exposure time [days]
+        exposure time [s]
 
     scale_factor : float
         constant factor to multiply flux by
@@ -126,16 +123,16 @@ def pdot_phasefold(t_obs, p0, pdot, t_zero=0):
     Parameters
     ==========
     times : array_like
-        array of observation times [days]
+        array of observation times [s]
 
     p0 : float
-        starting orbital period [days]
+        starting orbital period [s]
 
     pdot : float
-        rate of change of period
+        rate of change of period [s/s]
 
     t_zero : float
-        starting time of observation [days]
+        starting time of observation [s]
 
     Returns
     =======
@@ -149,7 +146,7 @@ def pdot_phasefold(t_obs, p0, pdot, t_zero=0):
     return np.mod(t_obs - 1/2*(pdot/p0)*t_obs**2, p0) / p0
 
 
-def periodfind(binary, observation):
+def periodfind(binary, observation, lightcurve):
     """
     Function which recovers the period for a given set of observations
 
@@ -161,22 +158,32 @@ def periodfind(binary, observation):
     observation : Observation
         binary observation object
 
+    lightcurve : dict
+        dictionary of lightcurve parameter
+
     Returns
     =======
     period : float
-        recovered orbital period [days]
+        recovered orbital period [s]
 
     period_err : float
-        error in recovered orbital period [days]
+        error in recovered orbital period [s]
     """
     # Set up the full set of injection_parameters
     injection_parameters = DEFAULT_INJECTION_PARAMETERS
-    injection_parameters["period"] = binary.p0
-    injection_parameters["t_zero"] = observation.obstimes[0]
-    injection_parameters["q"] = binary.q
-    injection_parameters["radius_1"] = binary.r1
-    injection_parameters["radius_2"] = binary.r2
-    injection_parameters["pdot"] = binary.pdot
+    injection_parameters['period'] = binary.p0
+    injection_parameters['t_zero'] = observation.obstimes[0]
+    injection_parameters['q'] = binary.q
+    injection_parameters['radius_1'] = binary.r1
+    injection_parameters['radius_2'] = binary.r2
+    injection_parameters['sbratio'] = lightcurve['sbratio']
+    injection_parameters['ldc_1'] = lightcurve['ldc_1']
+    injection_parameters['ldc_2'] = lightcurve['ldc_2']
+    injection_parameters['gdc_1'] = lightcurve['gdc_1']
+    injection_parameters['gdc_2'] = lightcurve['gdc_2']
+    injection_parameters['heat_1'] = lightcurve['heat_1']
+    injection_parameters['heat_2'] = lightcurve['heat_2']
+    injection_parameters['pdot'] = binary.pdot
 
     # Generate list of observation times
     t_obs = Observation(binary, t_zero=observation.obstimes[0],
@@ -193,7 +200,7 @@ def periodfind(binary, observation):
     pdots_to_test = np.array([0, binary.pdot]).astype(np.float32)
 
     # Normalize lightcurve and construct time and magnitude arrays
-    lc = (lc - np.min(lc)) / (np.max(lc)-np.min(lc))
+    lc = (lc - np.min(lc)) / (np.max(lc) - np.min(lc))
     time_stack = [t_obs.astype(np.float32)]
     mag_stack = [lc.astype(np.float32)]
 
@@ -236,15 +243,14 @@ def fdotgw(mchirp, p0):
         chirp mass [Solar Masses]
 
     p0 : float
-        starting orbital period [days]
+        starting orbital period [s]
 
     Returns
     =======
     fdot : float array
         rate of change of GW frequency [Hz/s]
     """
-    f0 = 2/p0 / (60*60*24)
-    return 96/5 * np.pi**(8/3) * (G * mchirp*MSUN / c**3)**(5/3) * f0**(11/3)
+    return 96/5 * np.pi**(8/3) * (G*mchirp*MSUN / c**3)**(5/3) * (2/p0)**(11/3)
 
 
 def chirp_mass(f0, fdot):
@@ -274,7 +280,7 @@ def semi_major_axis(p0, mchirp, q):
     Parameters
     ==========
     p0 : float
-        starting period [days]
+        starting period [s]
 
     mchirp : array_like
         chirp mass [Solar Masses]
@@ -287,7 +293,7 @@ def semi_major_axis(p0, mchirp, q):
     a : float array
         semi-major axis (mean separation) [km]
     """
-    return (G*mchirp*MSUN*((1+q)**2/q)**(3/5)*(p0*(60*60*24)/(2*np.pi))**2)**(1/3)/1000
+    return (G*mchirp*MSUN*((1+q)**2/q)**(3/5)*(p0/(2*np.pi))**2)**(1/3)/1000
 
 
 def q_minimum(mchirp):
@@ -315,7 +321,7 @@ def q_minimum(mchirp):
     return q_min
 
 
-def parameter_dict(binary, observation, lightcurve):
+def parameter_dict(binary, observation, lightcurve, pf_period=None, pf_period_err=None):
     """ 
     Function which organizes binary parameters into a dictionary structure
 
@@ -329,6 +335,9 @@ def parameter_dict(binary, observation, lightcurve):
 
     lightcurve : dict
         lightcurve parameters [sbratio, ldc_1, ldc_2, gdc_1, gdc_2, heat_1, heat_2]
+
+    periodfind: float array
+        period and uncertainty from periodfind if given
 
     Returns
     =======
@@ -347,14 +356,14 @@ def parameter_dict(binary, observation, lightcurve):
             'unit': 'Hz/s'
         },
         '$P_0$': {
-            'value': binary.p0*(60*24),
+            'value': binary.p0,
             'description': 'initial orbital period',
-            'unit': 'min'
+            'unit': 's'
         },
         '$\dot{P}$': {
             'value': binary.pdot,
             'description': 'time derivative of orbital period',
-            'unit': None
+            'unit': 's/s'
         },
         r'$\iota$': {
             'value': binary.incl,
@@ -385,6 +394,16 @@ def parameter_dict(binary, observation, lightcurve):
             'value': binary.m2,
             'description': 'mass of body 2 (secondary)',
             'unit': 'Solar Masses'
+        },
+        '$r_1$': {
+            'value': binary.r1,
+            'description': 'scaled radius of body 1',
+            'unit': None
+        },
+        '$r_2$': {
+            'value': binary.r2,
+            'description': 'scaled radius of body 2',
+            'unit': None
         },
         'a': {
             'value': binary.a,
@@ -459,12 +478,22 @@ def parameter_dict(binary, observation, lightcurve):
         '$t_0$': {
             'value': list(observation.phases),
             'description': 'reference epoch for each observation',
-            'unit': 'day'
+            'unit': 's'
         },
         'P': {
-            'value': list(observation.periods*(60*24)),
+            'value': list(observation.periods),
             'description': 'period at each observation time',
-            'unit': 'min'
+            'unit': 's'
+        },
+        r'$P_{pf}$': {
+            'value': float(pf_period),
+            'description': 'periodfind period',
+            'unit': 's'
+        },
+        r'$P_{pf,err}$': {
+            'value': float(pf_period_err),
+            'description': 'uncertainty of periodfind period',
+            'unit': 's'
         }
     }
     return parameter_dict
@@ -490,7 +519,7 @@ class KDE_Prior(bilby.core.prior.Prior):
         return self.kde.resample(1)
 
     def prob(self, val):
-        return self.kde.pdf(val)
+        return self.kde.pdf(val) * self.is_in_prior_range(val)
 
 
 class Uniform_Cosine_Prior(bilby.core.prior.Prior):
@@ -554,10 +583,10 @@ class Binary:
         Properties
         ==========
         p0 : float
-            starting orbital period [days]
+            starting orbital period [s]
 
         pdot : float
-            rate of change of orbital period
+            rate of change of orbital period [s/s]
 
         mchirp : float
             chirp mass [Solar Masses]
@@ -604,7 +633,7 @@ class Binary:
 
     @property
     def p0(self):
-        return 2 / self.f0 / (60*60*24)
+        return 2 / self.f0
 
     @property
     def pdot(self):
@@ -666,11 +695,11 @@ class Observation:
             binary object being observed
 
         t_obs : float array
-            array with observation times [days].
+            array with observation times [s].
             If specified, all other observation parameters are ignored.
 
         t_zero : float
-            starting time of observations [days]
+            starting time of observations [s]
 
         numobs : int
             number of observations
@@ -684,20 +713,20 @@ class Observation:
         Properties
         ==========
         obstimes : float array
-            observation times [days]
+            observation times [s]
 
         phases : float array
-            times of eclipses [days]
+            times of eclipses [s]
 
         periods : float array
-            orbital periods at observation times [days]
+            orbital periods at observation times [s]
         """
         self.binary = binary
         self.t_obs = t_obs
         self.t_zero = t_zero
         self.numobs = numobs
-        self.mean_dt = mean_dt
-        self.std_dt = std_dt
+        self.mean_dt = mean_dt*(24*60*60)
+        self.std_dt = std_dt*(24*60*60)
         if self.t_obs is None:
             t = np.full(self.numobs, self.t_zero)
             for i in range(1, len(t)):
@@ -713,9 +742,9 @@ class Observation:
     @property
     def phases(self):
         delta_t = self.obstimes - self.t_zero
-        return delta_t - 1/2*(self.binary.fdot/self.binary.f0)*(60*60*24)*delta_t**2
+        return delta_t - 1/2*(self.binary.fdot/self.binary.f0)*delta_t**2
 
     @property
     def periods(self):
-        tau = 3/8 * (self.binary.f0/self.binary.fdot) / (60*60*24)
-        return 2/self.binary.f0 * (1 - self.obstimes/tau)**(3/8) / (60*60*24)
+        tau = 3/8 * self.binary.f0 / self.binary.fdot
+        return 2/self.binary.f0 * (1 - self.obstimes/tau)**(3/8)

bilby_example/run_lightcurve_modeling.py
@@ -39,11 +39,11 @@ f0, fdot, incl = np.loadtxt(binary.joinpath(f'{binary.name}.dat'))[[0, 1, 5]]
 incl = 90 - np.abs(np.degrees(incl) - 90)
 
 # generate "true" mass-ratio between ~0.4-1.0 (m2/m1)
-np.random.seed(5*args.binary + 7)
+np.random.seed(7*args.binary + 11)
 b = Binary(f0, fdot, incl, 1)
 with warnings.catch_warnings():
     warnings.filterwarnings('ignore', category=RuntimeWarning)
-    q_min = max(0.4, q_minimum(b.mchirp))
+    q_min = max(0.25, q_minimum(b.mchirp))
 massratio = (1 - q_min)*np.random.rand() + q_min
 del b
 
@@ -59,12 +59,6 @@ b = Binary(f0, fdot, incl, massratio)
 o = Observation(b, numobs=args.numobs, mean_dt=args.mean_dt, std_dt=args.std_dt)
 t_obs, t_phase = o.obstimes, o.phases
 
-# Recover the period using the periodfind algorithm
-if args.periodfind:
-    period, period_err = periodfind(b, o)
-    del o
-    o = Observation(Binary(2/period/(60*60*24), fdot, incl, massratio), t_obs=t_obs)
-
 # Generate lightcurve specific parameters
 lcp = {}
 lcp['sbratio'] = 0.95 * np.random.rand() + 0.05
@@ -75,9 +69,16 @@ lcp['gdc_2'] = np.random.rand()
 lcp['heat_1'] = 5*np.random.rand()
 lcp['heat_2'] = 5*np.random.rand()
 
+# Recover the period using the periodfind algorithm
+period, period_err = None, None
+if args.periodfind:
+    period, period_err = periodfind(b, o, lcp)
+    del o
+    o = Observation(Binary(2/period, fdot, incl, massratio), t_obs=t_obs)
+
 # Write binary parameters to json file
 with open(binarydir.joinpath(f'{binary.name}_parameters.json'), 'w+') as parameters:
-    json.dump(parameter_dict(b, o, lcp), parameters, indent=2)
+    json.dump(parameter_dict(b, o, lcp, period, period_err), parameters, indent=2)
 
 
 t_0, P, q, inc = [], [], [], []
@@ -136,14 +137,14 @@ if not plotdir.is_dir():
 # Compute eclipse times and residuals
 t_0 = np.array(t_0, dtype=object)
 t_eclipse = np.array([np.median(t) for t in t_0])
-med_delta_t = np.array([np.median(r) for r in (t_obs - t_0)*(60*60*24)])
-std_delta_t = np.array([np.std(r) for r in (t_obs - t_0)*(60*60*24)])
+med_delta_t = np.array([np.median(r) for r in (t_obs - t_0)])
+std_delta_t = np.array([np.std(r) for r in (t_obs - t_0)])
 
 # Plot residuals against time
 plt.figure(figsize=(12, 8))
-plt.plot(t_eclipse, -1/2*(b.fdot/b.f0)*(t_eclipse*(60*60*24))**2, 'go', label="theory w/ fdot")
-plt.plot(t_eclipse, (t_eclipse-t_phase)*(60*60*24), 'r^', label="theory w/o fdot")
-plt.errorbar(t_eclipse, -med_delta_t, yerr=std_delta_t, fmt='kx', label="observed")
+plt.plot(t_eclipse/(60*60*24), -1/2*(b.fdot/b.f0)*t_eclipse**2, 'go', label="theory w/ fdot")
+plt.plot(t_eclipse/(60*60*24), (t_eclipse-t_phase), 'r^', label="theory w/o fdot")
+plt.errorbar(t_eclipse/(60*60*24), -med_delta_t, yerr=std_delta_t, fmt='kx', label="observed")
 plt.xlabel("time of observation [days]", fontsize=16)
 plt.ylabel(r"$\Delta t_{eclipse}$ [seconds]", fontsize=16)
 plt.legend()
@@ -153,7 +154,7 @@ plt.close()
 
 # Function to compute residuals from residual eclipse times, chirp mass, and period
 def res_model(delta_t, mchirp, period):
-    return (-1/2 * (fdotgw(mchirp, period)/2) * (delta_t*(60*60*24))**2) * period*(60*60*24)
+    return -1/2 * fdotgw(mchirp, period) * period/2 * delta_t**2
 
 # Set up the likelihood function
 likelihood = GaussianLikelihood(t_eclipse, -med_delta_t, res_model, std_delta_t)
@@ -163,13 +164,13 @@ injection = dict(mchirp=b.mchirp, period=b.p0)
 priors = bilby.core.prior.PriorDict()
 if args.gwprior:
     chaindata = np.loadtxt(binary.joinpath('chains/dimension_chain.dat.1'))
-    mchirp_prior_vals = chirp_mass(chaindata[:, 0], chaindata[:, 1])
-    priors['mchirp'] = KDE_Prior(mchirp_prior_vals, "mchirp",
-                                 latex_label=r"$\mathcal{M}$", unit=r"$M_{\odot}$")
+    mchirp_prior = chirp_mass(chaindata[:, 0], chaindata[:, 1])
+    priors['mchirp'] = KDE_Prior(mchirp_prior, "mchirp", latex_label=r"$\mathcal{M}$",
+                                 unit=r"$M_{\odot}$", minimum=0.05, maximum=1.25)
 else:
     priors['mchirp'] = Uniform(0.05, 1.25, "mchirp",
                                latex_label=r"$\mathcal{M}$", unit=r"$M_{\odot}$")
-priors['period'] = KDE_Prior(P[0], "period", latex_label="$P_0$", unit="days")
+priors['period'] = KDE_Prior(P[0], "period", latex_label="$P_0$", unit="s")
 
 result = bilby.run_sampler(likelihood=likelihood, priors=priors, sampler='pymultinest',
                            nlive=1000, outdir=plotdir, label=f'{binary.name}_mchirp')
@@ -187,7 +188,7 @@ if not args.periodfind:
 
 # Function to compute k2 from period, chirp mass, inclination, and mass ratio
 def k2_model(p0, mchirp, incl, q):
-    return 2*np.pi*semi_major_axis(p0, mchirp, q)*np.sin(np.radians(incl))/((1+q)*(p0*(60*60*24)))
+    return (2*np.pi/(p0*(1+q)))*semi_major_axis(p0, mchirp, q)*np.sin(np.radians(incl))
 
 # Set up likelihood function
 std_k2 = 50
@@ -196,8 +197,10 @@ likelihood = GaussianLikelihood(period, b.k2, k2_model, std_k2)
 # Set up priors and injection parameters
 injection = dict(mchirp=b.mchirp, incl=b.incl, q=b.q)
 priors = bilby.core.prior.PriorDict()
-priors['mchirp'] = KDE_Prior(mchirp, "mchirp", latex_label=r"$\mathcal{M}$", unit=r"$M_{\odot}$")
-priors['incl'] = KDE_Prior(inc[0], "incl", latex_label=r"$\iota$", unit="$^\circ$", minimum=0, maximum=90)
+priors['mchirp'] = KDE_Prior(mchirp, "mchirp", latex_label=r"$\mathcal{M}$",
+                             unit=r"$M_{\odot}$", minimum=0.05, maximum=1.25)
+priors['incl'] = KDE_Prior(inc[0], "incl", latex_label=r"$\iota$",
+                           unit="$^\circ$", minimum=0, maximum=90)
 priors['q'] = KDE_Prior(q[0], "q", latex_label="q", minimum=0.15, maximum=1)
 
 result = bilby.run_sampler(likelihood=likelihood, priors=priors, sampler='pymultinest',

bilby_example/simulate_lightcurve.py
@@ -8,8 +8,8 @@ from common import DEFAULT_INJECTION_PARAMETERS, basic_model
 parser = argparse.ArgumentParser()
 parser.add_argument("--outdir", help="path to the ouput directory")
 parser.add_argument("--label", type=str, help="label for the ouput lightcurve")
-parser.add_argument("--period", type=float, help="period [days]")
-parser.add_argument("--t-zero", type=float, help="t-zero")
+parser.add_argument("--period", type=float, help="period [s]")
+parser.add_argument("--t-zero", type=float, help="t-zero [s]")
 parser.add_argument("--incl", type=float, help="inclination [degrees]")
 parser.add_argument("--massratio", type=float, help="mass ratio (m2/m1)")
 parser.add_argument("--radius", type=float, nargs='+', help="radii (scaled)")
@@ -37,7 +37,7 @@ flux = data[:, 3] / np.max(data[:, 3])
 flux_err = args.error_mult * np.sqrt(data[:, 4]**2 + errorbudget**2) / np.max(data[:, 3])
 
 # Shift the times so that the mid-point is equal to t-zero
-t_obs = np.sort(data[:, 0] - (data[:, 0][0] + data[:, 0][-1])/2 + args.t_zero)
+t_obs = np.sort(data[:, 0] - (data[:, 0][0] + data[:, 0][-1])/2)*(24*60*60) + args.t_zero
 
 # Set up the full set of injection parameters
 injection_parameters = DEFAULT_INJECTION_PARAMETERS
@@ -62,13 +62,13 @@ flux = basic_model(t_obs, **injection_parameters)
 # Write the lightcurve to file
 lightcurve_data = np.array([t_obs, flux, flux_err]).T
 np.savetxt(Path(args.outdir).joinpath(f'{label}.dat'), lightcurve_data,
-        fmt='%.15g', header="MJD flux fluxerr")
+        fmt='%.15g', header="time flux fluxerr")
 
 # Generate a plot of the data
 plt.figure(figsize=(12, 8))
-plt.xlim([args.t_zero, args.t_zero+0.1])
+plt.xlim([args.t_zero, args.t_zero + 0.1*(24*60*60)])
 plt.ylim([0, 0.04])
-plt.xlabel("time [days]", fontsize=18, labelpad=10)
+plt.xlabel("time [s]", fontsize=18, labelpad=10)
 plt.ylabel("flux", fontsize=18, labelpad=10)
 plt.plot(t_obs, flux, zorder=3)
 plt.errorbar(t_obs, flux, flux_err)